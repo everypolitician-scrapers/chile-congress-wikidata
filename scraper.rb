96eb4f4d28aaef34f0dd34143841cc3b7ff485be
Tony Bowden <tony@tmtm.com>
2016-02-27 16:16:02 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'pry'
-require 'rest-client'
 
 @pages = [
   'Categoría:Diputados del XLVIII Periodo Legislativo del Congreso Nacional de Chile',
@@ -16,10 +13,5 @@ require 'rest-client'
   'Categoría:Diputados del LIV Periodo Legislativo del Congreso Nacional de Chile',
 ]
 
-ids = @pages.map { |c| WikiData::Category.new(c, 'es').wikidata_ids }.flatten.uniq
-ids.each_with_index do |id, i|
-  puts i if (i % 50).zero?
-  data = WikiData::Fetcher.new(id: id).data('es') or next
-  ScraperWiki.save_sqlite([:id], data) rescue binding.pry
-end
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = @pages.map { |c| WikiData::Category.new(c, 'es').member_titles }.flatten.uniq
+EveryPolitician::Wikidata.scrape_wikidata(names: { es: names })
